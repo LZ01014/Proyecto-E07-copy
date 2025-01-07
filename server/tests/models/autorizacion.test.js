e33a6bf86e7c118bf194da1c1942c3b96463b699
Lihao Zhu <72415942+LZ01014@users.noreply.github.com>
2025-01-07 21:40:47 +0000

    Update autorizacion.test.js

--- server/tests/models/autorizacion.test.js
+++ server/tests/models/autorizacion.test.js
@@ -1,207 +1,210 @@
 process.env.NODE_ENV = 'test';
 
 require('dotenv').config();
-const chai = require('chai');
 const mongoose = require('mongoose');
 const Autorizacion = require('../../models/Autorizacion');
-const { expect } = chai;
 
-describe('Autorizacion Model', () => {
-  before(async () => {
-    const dbUri = process.env.MONGODB_TEST_URI;
-    if (!dbUri) {
-      throw new Error('MONGODB_TEST_URI no está definido');
-    }
-    await mongoose.connect(dbUri);
-  });
+(async () => {
+  const chai = await import('chai');
+  const { expect } = chai;
 
-  beforeEach(async () => {
-    await Autorizacion.deleteMany({});
-  });
-
-  after(async () => {
-    await Autorizacion.deleteMany({});
-    await mongoose.connection.close();
-  });
-
-  it('should create a valid autorizacion', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: new mongoose.Types.ObjectId(),
-      pacienteId: new mongoose.Types.ObjectId(),
-      citaId: new mongoose.Types.ObjectId(),
-      autorizado: true
+  describe('Autorizacion Model', () => {
+    before(async () => {
+      const dbUri = process.env.MONGODB_TEST_URI;
+      if (!dbUri) {
+        throw new Error('MONGODB_TEST_URI no está definido');
+      }
+      await mongoose.connect(dbUri);
     });
 
-    const savedAutorizacion = await autorizacion.save();
-    expect(savedAutorizacion._id).to.exist;
-    expect(savedAutorizacion.autorizado).to.be.true;
-  });
-
-  it('should not create an autorizacion without prestacionId', async () => {
-    const autorizacion = new Autorizacion({
-      pacienteId: new mongoose.Types.ObjectId(),
-      citaId: new mongoose.Types.ObjectId(),
-      autorizado: true
+    beforeEach(async () => {
+      await Autorizacion.deleteMany({});
     });
 
-    try {
-      await autorizacion.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.errors.prestacionId).to.exist;
-      expect(error.errors.prestacionId.message).to.equal('El ID de la prestación es obligatorio.');
-    }
-  });
-
-  it('should not create an autorizacion with invalid prestacionId', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: 'invalid-id',
-      pacienteId: new mongoose.Types.ObjectId(),
-      citaId: new mongoose.Types.ObjectId(),
-      autorizado: true
+    after(async () => {
+      await Autorizacion.deleteMany({});
+      await mongoose.connection.close();
     });
 
-    try {
-      await autorizacion.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.errors.prestacionId).to.exist;
-      expect(error.errors.prestacionId.message).to.equal('Cast to ObjectId failed for value "invalid-id" (type string) at path "prestacionId" because of "BSONError"');
-    }
-  });
+    it('should create a valid autorizacion', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: new mongoose.Types.ObjectId(),
+        pacienteId: new mongoose.Types.ObjectId(),
+        citaId: new mongoose.Types.ObjectId(),
+        autorizado: true
+      });
 
-  it('should not create an autorizacion without pacienteId', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: new mongoose.Types.ObjectId(),
-      citaId: new mongoose.Types.ObjectId(),
-      autorizado: true
+      const savedAutorizacion = await autorizacion.save();
+      expect(savedAutorizacion._id).to.exist;
+      expect(savedAutorizacion.autorizado).to.be.true;
     });
 
-    try {
-      await autorizacion.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.errors.pacienteId).to.exist;
-      expect(error.errors.pacienteId.message).to.equal('El ID del paciente es obligatorio.');
-    }
-  });
+    it('should not create an autorizacion without prestacionId', async () => {
+      const autorizacion = new Autorizacion({
+        pacienteId: new mongoose.Types.ObjectId(),
+        citaId: new mongoose.Types.ObjectId(),
+        autorizado: true
+      });
 
-  it('should not create an autorizacion with invalid pacienteId', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: new mongoose.Types.ObjectId(),
-      pacienteId: 'invalid-id',
-      citaId: new mongoose.Types.ObjectId(),
-      autorizado: true
+      try {
+        await autorizacion.save();
+      } catch (error) {
+        expect(error).to.exist;
+        expect(error.errors.prestacionId).to.exist;
+        expect(error.errors.prestacionId.message).to.equal('El ID de la prestación es obligatorio.');
+      }
     });
 
-    try {
-      await autorizacion.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.errors.pacienteId).to.exist;
-      expect(error.errors.pacienteId.message).to.equal('Cast to ObjectId failed for value "invalid-id" (type string) at path "pacienteId" because of "BSONError"');
-    }
-  });
+    it('should not create an autorizacion with invalid prestacionId', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: 'invalid-id',
+        pacienteId: new mongoose.Types.ObjectId(),
+        citaId: new mongoose.Types.ObjectId(),
+        autorizado: true
+      });
 
-  it('should not create an autorizacion without citaId', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: new mongoose.Types.ObjectId(),
-      pacienteId: new mongoose.Types.ObjectId(),
-      autorizado: true
+      try {
+        await autorizacion.save();
+      } catch (error) {
+        expect(error).to.exist;
+        expect(error.errors.prestacionId).to.exist;
+        expect(error.errors.prestacionId.message).to.equal('Cast to ObjectId failed for value "invalid-id" (type string) at path "prestacionId" because of "BSONError"');
+      }
     });
 
-    try {
-      await autorizacion.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.errors.citaId).to.exist;
-      expect(error.errors.citaId.message).to.equal('El ID de la cita es obligatorio.');
-    }
-  });
+    it('should not create an autorizacion without pacienteId', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: new mongoose.Types.ObjectId(),
+        citaId: new mongoose.Types.ObjectId(),
+        autorizado: true
+      });
 
-  it('should not create an autorizacion with invalid citaId', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: new mongoose.Types.ObjectId(),
-      pacienteId: new mongoose.Types.ObjectId(),
-      citaId: 'invalid-id',
-      autorizado: true
+      try {
+        await autorizacion.save();
+      } catch (error) {
+        expect(error).to.exist;
+        expect(error.errors.pacienteId).to.exist;
+        expect(error.errors.pacienteId.message).to.equal('El ID del paciente es obligatorio.');
+      }
     });
 
-    try {
-      await autorizacion.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.errors.citaId).to.exist;
-      expect(error.errors.citaId.message).to.equal('Cast to ObjectId failed for value "invalid-id" (type string) at path "citaId" because of "BSONError"');
-    }
-  });
+    it('should not create an autorizacion with invalid pacienteId', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: new mongoose.Types.ObjectId(),
+        pacienteId: 'invalid-id',
+        citaId: new mongoose.Types.ObjectId(),
+        autorizado: true
+      });
 
-  it('should not create an autorizacion without autorizado', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: new mongoose.Types.ObjectId(),
-      pacienteId: new mongoose.Types.ObjectId(),
-      citaId: new mongoose.Types.ObjectId()
+      try {
+        await autorizacion.save();
+      } catch (error) {
+        expect(error).to.exist;
+        expect(error.errors.pacienteId).to.exist;
+        expect(error.errors.pacienteId.message).to.equal('Cast to ObjectId failed for value "invalid-id" (type string) at path "pacienteId" because of "BSONError"');
+      }
     });
 
-    try {
-      await autorizacion.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.errors.autorizado).to.exist;
-      expect(error.errors.autorizado.message).to.equal('El estado de autorización es obligatorio.');
-    }
-  });
+    it('should not create an autorizacion without citaId', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: new mongoose.Types.ObjectId(),
+        pacienteId: new mongoose.Types.ObjectId(),
+        autorizado: true
+      });
 
-  it('should create an autorizacion with autorizado set to false', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: new mongoose.Types.ObjectId(),
-      pacienteId: new mongoose.Types.ObjectId(),
-      citaId: new mongoose.Types.ObjectId(),
-      autorizado: false
+      try {
+        await autorizacion.save();
+      } catch (error) {
+        expect(error).to.exist;
+        expect(error.errors.citaId).to.exist;
+        expect(error.errors.citaId.message).to.equal('El ID de la cita es obligatorio.');
+      }
     });
 
-    const savedAutorizacion = await autorizacion.save();
-    expect(savedAutorizacion._id).to.exist;
-    expect(savedAutorizacion.autorizado).to.be.false;
-  });
+    it('should not create an autorizacion with invalid citaId', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: new mongoose.Types.ObjectId(),
+        pacienteId: new mongoose.Types.ObjectId(),
+        citaId: 'invalid-id',
+        autorizado: true
+      });
 
-  it('should create an autorizacion without autorizado (default to false)', async () => {
-    const autorizacion = new Autorizacion({
-      prestacionId: new mongoose.Types.ObjectId(),
-      pacienteId: new mongoose.Types.ObjectId(),
-      citaId: new mongoose.Types.ObjectId()
+      try {
+        await autorizacion.save();
+      } catch (error) {
+        expect(error).to.exist;
+        expect(error.errors.citaId).to.exist;
+        expect(error.errors.citaId.message).to.equal('Cast to ObjectId failed for value "invalid-id" (type string) at path "citaId" because of "BSONError"');
+      }
     });
 
-    const savedAutorizacion = await autorizacion.save();
-    expect(savedAutorizacion._id).to.exist;
-    expect(savedAutorizacion.autorizado).to.be.false;
-  });
+    it('should not create an autorizacion without autorizado', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: new mongoose.Types.ObjectId(),
+        pacienteId: new mongoose.Types.ObjectId(),
+        citaId: new mongoose.Types.ObjectId()
+      });
 
-  it('should not create an autorizacion with duplicate prestacionId for the same pacienteId and citaId', async () => {
-    const prestacionId = new mongoose.Types.ObjectId();
-    const pacienteId = new mongoose.Types.ObjectId();
-    const citaId = new mongoose.Types.ObjectId();
-
-    const autorizacion1 = new Autorizacion({
-      prestacionId,
-      pacienteId,
-      citaId,
-      autorizado: true
-    });
-    await autorizacion1.save();
-
-    const autorizacion2 = new Autorizacion({
-      prestacionId,
-      pacienteId,
-      citaId,
-      autorizado: false
+      try {
+        await autorizacion.save();
+      } catch (error) {
+        expect(error).to.exist;
+        expect(error.errors.autorizado).to.exist;
+        expect(error.errors.autorizado.message).to.equal('El estado de autorización es obligatorio.');
+      }
     });
 
-    try {
-      await autorizacion2.save();
-    } catch (error) {
-      expect(error).to.exist;
-      expect(error.message).to.equal('Ya existe una receta para este médico y paciente en la misma fecha y hora');
-    }
+    it('should create an autorizacion with autorizado set to false', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: new mongoose.Types.ObjectId(),
+        pacienteId: new mongoose.Types.ObjectId(),
+        citaId: new mongoose.Types.ObjectId(),
+        autorizado: false
+      });
+
+      const savedAutorizacion = await autorizacion.save();
+      expect(savedAutorizacion._id).to.exist;
+      expect(savedAutorizacion.autorizado).to.be.false;
+    });
+
+    it('should create an autorizacion without autorizado (default to false)', async () => {
+      const autorizacion = new Autorizacion({
+        prestacionId: new mongoose.Types.ObjectId(),
+        pacienteId: new mongoose.Types.ObjectId(),
+        citaId: new mongoose.Types.ObjectId()
+      });
+
+      const savedAutorizacion = await autorizacion.save();
+      expect(savedAutorizacion._id).to.exist;
+      expect(savedAutorizacion.autorizado).to.be.false;
+    });
+
+    it('should not create an autorizacion with duplicate prestacionId for the same pacienteId and citaId', async () => {
+      const prestacionId = new mongoose.Types.ObjectId();
+      const pacienteId = new mongoose.Types.ObjectId();
+      const citaId = new mongoose.Types.ObjectId();
+
+      const autorizacion1 = new Autorizacion({
+        prestacionId,
+        pacienteId,
+        citaId,
+        autorizado: true
+      });
+      await autorizacion1.save();
+
+      const autorizacion2 = new Autorizacion({
+        prestacionId,
+        pacienteId,
+        citaId,
+        autorizado: false
+      });
+
+      try {
+        await autorizacion2.save();
+      } catch (error) {
+        expect(error).to.exist;
+        expect(error.message).to.equal('Ya existe una receta para este médico y paciente en la misma fecha y hora');
+      }
+    });
   });
-});
+})();
